9a89fbefa9fa74ef2662c0870e062198f84cd142
Kevin <kevin.souza.94@gmail.com>
2021-05-31 15:11:26 -0300

    feat: criar routes profissional
    Cria as rotas para o profissional

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\ProcedureController;
+use App\Http\Controllers\ProfessionalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,9 @@ Route::get('procedures/{id}', [ProcedureController::class, 'show']);
 Route::post('procedures', [ProcedureController::class, 'store']);
 Route::put('procedures/{id}', [ProcedureController::class, 'update']);
 Route::delete('procedures/{id}', [ProcedureController::class, 'delete']);
+
+Route::get('professionals', [ProfessionalController::class, 'index']);
+Route::get('professionals/{id}', [ProfessionalController::class, 'show']);
+Route::post('professionals', [ProfessionalController::class, 'store']);
+Route::put('professionals/{id}', [ProfessionalController::class, 'update']);
+Route::delete('professionals/{id}', [ProfessionalController::class, 'delete']);
