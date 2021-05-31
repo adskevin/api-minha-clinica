e36df0658b361ea725490a70799e01294466c2e6
Kevin <kevin.souza.94@gmail.com>
2021-05-31 02:09:45 -0300

    feat: criar migration profissional
    Cria migration e model para profissional.

---
+++ app/Models/Professional.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Professional extends Model
+{
+    protected $fillable = ['nome', 'cargo'];
+    use HasFactory;
+}
