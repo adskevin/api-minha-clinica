84c9de41bd9451c07777360dc104e42b06e817b9
Kevin <kevin.souza.94@gmail.com>
2021-05-30 23:10:37 -0300

    feat: criar routes paciente
    Cria as rotas para o paciente.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('patients', [PatientController::class, 'index']);
+Route::get('patients/{id}', [PatientController::class, 'show']);
+Route::post('patients', [PatientController::class, 'store']);
+Route::put('patients/{id}', [PatientController::class, 'update']);
+Route::delete('patients/{id}', [PatientController::class, 'delete']);
