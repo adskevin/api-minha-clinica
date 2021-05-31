8e921bfe23497ffc7c747894a2d0e8d09dcfa113
Kevin <kevin.souza.94@gmail.com>
2021-05-30 23:06:04 -0300

    feat: criar migration paciente
    Cria migration, model e seeder para o paciente

---
+++ database/seeders/PatientSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Patient;
+
+class PatientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Patient::truncate();
+
+        $faker = \Faker\Factory::create('pt_BR');
+
+        for ($i = 0; $i < 5; $i++) {
+            Patient::create([
+                'nome' => $faker->name,
+                'telefone' => $faker->cellphone(false),
+            ]);
+        }
+    }
+}
